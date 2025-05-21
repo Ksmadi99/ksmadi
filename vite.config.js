7471c1837954952b2adb85123921fef0f5985725
Ksmadi99 <kasmadianahudaumar@gmail.com>
2025-05-21 16:37:29 +0800

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+console.log('Alias @ resolves to:', path.resolve(__dirname, './src'))
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),  // Make sure this points exactly to ./src
+      '@': path.resolve(__dirname, './src'),
     },
   },
-  console.log('Alias @ resolves to:', path.resolve(__dirname, './src'))
 })
